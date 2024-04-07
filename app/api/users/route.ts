2dffe1320b05dba00eaab49727ab097bece2bdd7
root <root@Unaislaptop>
2024-04-07 17:46:36 +0530

    Now my frontend nextjs app talking to backend nextjs app

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,4 +1,11 @@
-export function GET() {
+ export function GET() {
+    return Response.json({
+        name : "Unais",
+        email : "unais5676@gmail.com"
+    })
+}
+
+export function POST() {
     return Response.json({
         name : "Unais",
         email : "unais5676@gmail.com"
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 
 const getUserDetails = async() =>{
-  const promise = await new Promise((r) => setTimeout(r,5000))
-  const response = await axios.get('https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details');
+  
+  const response = await axios.get('http://localhost:3000/api/users');
   return response.data
 }
 export default async function Home() {
